a6dcd99a792f6886cf52993fb351137008e8f7f1
Daniel Mass Dominguez <danielmassdominguez@gmail.com>
2023-08-14 13:36:17 -0600

    The program logic starts to be written in a specific class, the requestValueToConvert method is added

--- src/com/conversor/Logic.java
+++ src/com/conversor/Logic.java
@@ -1,5 +1,75 @@
 package com.conversor;
 
-public class Logic {
+import javax.swing.JOptionPane;
 
+public class Logic {
+	
+	private static String[] mainOptions= {
+			 "Divisas",
+			 "Temperatura"
+	};
+	
+	public static String requestMainOption() {
+	
+		String choosedOption = (String) JOptionPane.showInputDialog(
+		          null,
+		          "Selecciona lo que deseas convertir",
+		          "Conversor",
+		          JOptionPane.QUESTION_MESSAGE,
+		          null,
+		          mainOptions,
+		          mainOptions[0]);
+
+		if (choosedOption != null) {
+
+		      JOptionPane.showMessageDialog(
+		              null,
+		              "La opcion seleccionada fue: " + choosedOption,
+		              "Conversor",
+		              JOptionPane.INFORMATION_MESSAGE);
+		      return choosedOption;
+		      
+		} else {
+		      JOptionPane.showMessageDialog(
+		              null,
+		              "Ninguna opcion seleccionada, cerrando programa",
+		              "Conversor",
+		              JOptionPane.WARNING_MESSAGE);
+		      return null;
+		  }
+		
+	}
+	
+	public static String RequestValueToconvert(String choosedOption) {
+		
+		String choosedValue = (String) JOptionPane.showInputDialog(
+		          null,
+		          "Inserte la cantidad a convertir",
+		          "Conversor de " + choosedOption,
+		          JOptionPane.QUESTION_MESSAGE,
+		          null,
+		          null,
+		          "0");
+
+		if (choosedValue != null) {
+
+		      JOptionPane.showMessageDialog(
+		              null,
+		              "La cantidad ingresada fue: " + choosedValue,
+		              "Conversor de " + choosedOption,
+		              JOptionPane.INFORMATION_MESSAGE);
+		      return choosedValue;
+		      
+		} else {
+		      JOptionPane.showMessageDialog(
+		              null,
+		              "No se ingreso ningun nuemero, cerrando programa.",
+		              "Conversor de " + choosedOption,
+		              JOptionPane.WARNING_MESSAGE);
+		      return null;
+		  }
+		
+	}
+	
+	
 }
--- src/com/conversor/Main.java
+++ src/com/conversor/Main.java
@@ -6,35 +6,10 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		String[] mainOptions= {
-				 "Divisas",
-				 "Temperatura"
-		};
-		
-		String choosedOption = (String) JOptionPane.showInputDialog(
-               null,
-               "Selecciona lo que deseas convertir",
-               "Conversor",
-               JOptionPane.QUESTION_MESSAGE,
-               null,
-               mainOptions,
-               mainOptions[0]);
-
-       if (choosedOption != null) {
-
-           JOptionPane.showMessageDialog(
-                   null,
-                   "La opcion seleccionada fue: " + choosedOption,
-                   "Conversor",
-                   JOptionPane.INFORMATION_MESSAGE);
-       } else {
-           JOptionPane.showMessageDialog(
-                   null,
-                   "Ninguna opcion seleccionada, cerrando programa",
-                   "Conversor",
-                   JOptionPane.WARNING_MESSAGE);
-       }
+		String choosedMainOption = Logic.requestMainOption();
        
+		String value = Logic.RequestValueToconvert(choosedMainOption);
+		
 	}
 
 }
